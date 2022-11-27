2833dc70b03dbaaeb005e2554f2bf025d1d53149
nurdiansyahhamidi <nurdiansyahhamidi@gmail.com>
2022-11-27 18:52:40 +0700

    commit

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -8,7 +8,7 @@ const SearchItem = () => {
             <h1 className="siTitle">Green Resident Apartments</h1>
             <span className="siDistance">200m from center</span>
             <span className="siTaxiOp">Free airport taxi</span>
-            <span className="siSubstitle">
+            <span className="siSubtitle">
                 Studio Apartment with Air Conditioning
             </span>
             <span className="siFeatures">Entire studio + 1 bathroom + 21m 1 full bed</span>
@@ -17,7 +17,17 @@ const SearchItem = () => {
                 You can cancel later, so lock in this great price today!
             </span>
         </div>
-        <div className="siDetails">Details</div>
+        <div className="siDetails">
+          <div className="siRating">
+            <span>Exellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="siDetailTexts">
+            <span className="siPrice">$153</span>
+            <span className="siTaxOp">Includes taxes and fees</span>
+            <button className="siCheckButton">See Avaibility</button>
+          </div>
+        </div>
     </div>
   )
 }
